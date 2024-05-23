d1a7a2d55a4758e0f0ffe491bc2221eab63969b1
Eric Miller <51423770+erm156@users.noreply.github.com>
2024-05-23 17:24:08 -0400

    Revert "migrate external data fetching code from interoperation"

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import logger from "./utils/logger.js";
-import fetchData from "./fetchData.js";
-
-logger.info("starting data fetching...");
-await fetchData.mapExternalDataToStudies();
-logger.info("data fetching complete!");
+console.log("Hello world");
